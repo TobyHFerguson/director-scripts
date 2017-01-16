fd93f0dcc5a55ac5a9f9d2db888c5ca1fe838e83
TobyHFerguson <toby@toby-MBP.local>
2017-01-16 15:27:33 -0800

    Major directory refactoring
    Made the output directory more streamlined and abstract
    
    There's now the notion of a base directory with specific directories for
    specific owners, and all of the artifacts used for that owner session
    are in that subdirectory structure.

--- transient-aws/create_director_image.sh
+++ transient-aws/create_director_image.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 # $1 - AWS_ACCESS_KEY_ID
 # $2 - AWS_SECRET_ACCESS_KEY
+# $3 - Owner - optional; defaults to user running script
+# $4 - base directory for cloud lab - optional
 export AWS_ACCESS_KEY_ID=${1:?}
 export AWS_SECRET_ACCESS_KEY=${2:?}
 OWNER=${3:?}
+CLOUD_LAB_DIR=${4:-/tmp/cloud_lab}
+
 
 export AWS_DEFAULT_REGION=us-east-1
 
 # CONSTANTS
+
 CLUSTER_CDH_AMI=ami-49e9fe5e	# AMI for pre-created CDH image
 CLUSTER_OS_USER=centos		# User to ssh to CDH image
 DIRECTOR_OS_AMI=ami-0ca23e1b	# AMI to use for Director - RHEL 73
@@ -17,11 +22,6 @@ DIRECTOR_INSTANCE_TYPE=c4.xlarge # Director instance type
 INSTANCENAME=${OWNER:?}-director	 # Name for Director instance
 SECURITY_GROUP=sg-891a50f1	 # Security group controlling the cluster
 SUBNET_ID=subnet-e7542291	 # Subnet within which the cluster will run
-
-# Create a directory to put the ssh information in
-SSH_DIR=/tmp/cloud-lab-${OWNER:?}
-rm -rf ${SSH_DIR:?}
-mkdir -p ${SSH_DIR:?}
 
 function random() {
     SEED="$(date) $RANDOM"
@@ -55,10 +55,26 @@ function create-uniq-bucket() {
     echo $bn
 }
 
+# Construct the output directory structures and file names
+BASE_DIR=${CLOUD_LAB_DIR:?}/${OWNER:?}.$$/ # base for this specific session with this specific owner
+OUTPUT_DIR=${BASE_DIR:?}/output		   # output directory used to create zip file
+STAGE_DIR=${BASE_DIR:?}/stage		   # stage directory used to expand template files
+TMP_DIR=${BASE_DIR:?}/tmp		   # tmp directory for scripts and other calculated items
+mkdir -p ${OUTPUT_DIR:?}
+mkdir -p ${STAGE_DIR:?}
+mkdir -p ${TMP_DIR:?}
+
+# temp directory
+SSH_PRIVATE_KEY=${TMP_DIR:?}/private_key # the private key made into an easily substituted string
+SED_COMMANDS_FILE=${TMP_DIR:?}/commands.sed # a file containing the template expansion commands
+
+# Output directory
+SSH_CONFIG_FILE=${OUTPUT_DIR:?}/ssh_config # The config file for owner to use
+README=${OUTPUT_DIR:?}/README		   # The README for the user
 
 # # construct a brand new key pair and put the private key into a file
 AWS_KEYNAME=$(make-key-name)
-AWS_SSH_KEYFILE=${SSH_DIR:?}/${AWS_KEYNAME:?}
+AWS_SSH_KEYFILE=${OUTPUT_DIR:?}/${AWS_KEYNAME:?}
 touch ${AWS_SSH_KEYFILE:?}
 chmod 600 ${AWS_SSH_KEYFILE:?}
 aws ec2 create-key-pair --output text --key-name ${AWS_KEYNAME:?} --query 'KeyMaterial' >${AWS_SSH_KEYFILE:?} || {
@@ -68,6 +84,7 @@ aws ec2 create-key-pair --output text --key-name ${AWS_KEYNAME:?} --query 'KeyMa
     exit 2
     }
 
+# Get the various resources from the cloud provider
 INSTANCE_ID=$(aws ec2 run-instances --image-id ${DIRECTOR_OS_AMI:?} --count 1 --instance-type ${DIRECTOR_INSTANCE_TYPE:?} --key-name ${AWS_KEYNAME:?} --security-group-ids ${SECURITY_GROUP:?} --subnet-id ${SUBNET_ID:?} --disable-api-termination --output text | grep INSTANCES | cut -f 8)
 aws ec2 create-tags --resources ${INSTANCE_ID:?} --tags Key=owner,Value=${OWNER:?} Key=Name,Value=${INSTANCENAME:?}
 message "Created instance named ${INSTANCENAME:?}, id: ${INSTANCE_ID:?} tagged with owner = ${OWNER:?}. 
@@ -88,13 +105,14 @@ SECURITY_GROUP_ID=$(aws ec2 describe-instances --instance-ids ${INSTANCE_ID:?} -
 BUCKET_NAME=$(create-uniq-bucket)
 message "created bucket s3://${BUCKET_NAME:?}/"
 
+# Create as much as possible on local disk before operating on the remote instance
 
-# anything could be in here
-SSH_PRIVATE_KEY=/tmp/pk.$$
+# Create the temp directory files
+## The private key
 sed -e 's|\(-----BEGIN RSA PRIVATE KEY-----\)|    privateKey: """\1|' -e 's|\(-----END RSA PRIVATE KEY-----\)|\1"""|' ${AWS_SSH_KEYFILE:?} >${SSH_PRIVATE_KEY:?}
 
-
-cat - >/tmp/commands.sed <<EOF
+## the sed command file
+cat - > ${SED_COMMANDS_FILE:?} <<EOF
 s|REPLACE_ME_AWS_ACCESS_KEY_ID|${AWS_ACCESS_KEY_ID:?}|g
 s|REPLACE_ME_AWS_SECRET_ACCESS_KEY|${AWS_SECRET_ACCESS_KEY:?}|g
 s|REPLACE_ME_BUCKET_NAME|${BUCKET_NAME:?}|g
@@ -112,11 +130,8 @@ s|REPLACE_ME_SUBNET_ID|${SUBNET_ID:?}|g
 EOF
 
 
-STAGE_DIR=/tmp/create_director.$$
-mkdir /tmp/create_director.$$
-
-
-SSH_CONFIG_FILE=${SSH_DIR:?}/ssh_config
+# Create the output directory files
+# The ssh config file
 cat - >${SSH_CONFIG_FILE:?} <<EOF
 Host director
      Hostname ${DIRECTOR_IP_ADDRESS:?}
@@ -126,34 +141,9 @@ Host director
      StrictHostKeyChecking no
 EOF
 
-# Substitute for the variables into the staging files
-for file in hive-example/*
-do
-    sed -f /tmp/commands.sed $file >${STAGE_DIR:?}/$(basename $file)
-done
-# Make the shell scripts executable
-chmod a+x ${STAGE_DIR:?}/*.sh
 
-# Wait until we have ssh access
-message "Waiting for ssh access to instance id: ${INSTANCE_ID:?}"
-until ssh -q  -F ${SSH_CONFIG_FILE:?} director 'echo hi >/dev/null'; do message "Waiting for ssh access to instance id: ${INSTANCE_ID:?}"; sleep 10; done
-
-message "Copying files to the director instance"
-# copy over the keyfile
-scp -qF ${SSH_CONFIG_FILE:?} ${AWS_SSH_KEYFILE:?} director:.ssh/id_rsa
-
-# Make the aws directory on the director
-ssh -qtF ${SSH_CONFIG_FILE:?} director 'mkdir -p ~/.aws'
-
-# Copy the expanded credential and config file over and then delete them from the staging directory
-for file in ${STAGE_DIR:?}/{config,credentials}; do scp -F ${SSH_CONFIG_FILE} $file director:.aws/; rm -f $file; done
-
-# Copy over the remaining files from the staging directory into the director's home directory.
-for file in ${STAGE_DIR:?}/* install_director.sh ; do scp  -F ${SSH_CONFIG_FILE:?} $file director:; done
-
-# Publish the text file
-
-cat - >${SSH_DIR:?}/README <<EOF
+## The README
+cat - >${README:?} <<EOF
 ssh
    ssh configuration to access your director instance is in ssh_config
    The private keyfile for your director instance is in ${AWS_KEYNAME:?}
@@ -174,21 +164,42 @@ Executing the ETL Job
 
 EOF
 
-# Edit the IdentityFile location to make it local to the ${SSH_DIR:?}
-sed -i '' s@${SSH_DIR:?}/@@ ${SSH_CONFIG_FILE:?}
+
+# Expand the templates
+
+# Substitute for the variables into the staging files
+for file in hive-example/*
+do
+    sed -f ${SED_COMMANDS_FILE:?} $file >${STAGE_DIR:?}/$(basename $file)
+done
+# Make the shell scripts executable
+chmod a+x ${STAGE_DIR:?}/*.sh
+
+# Copy files to the director instance
+## Wait until we have ssh access
+message "Waiting for ssh access to instance id: ${INSTANCE_ID:?}"
+until ssh -q  -F ${SSH_CONFIG_FILE:?} director 'echo hi >/dev/null'; do message "Waiting for ssh access to instance id: ${INSTANCE_ID:?}"; sleep 10; done
+
+
+message "Copying files to the director instance"
+## copy over the keyfile
+scp -qF ${SSH_CONFIG_FILE:?} ${AWS_SSH_KEYFILE:?} director:.ssh/id_rsa
+
+## Make the aws directory on the director
+ssh -qtF ${SSH_CONFIG_FILE:?} director 'mkdir -p ~/.aws'
+
+## Copy the expanded credential and config file over and then delete them from the staging directory
+scp -F ${SSH_CONFIG_FILE} ${STAGE_DIR:?}/{config,credentials} director:.aws/ &&
+rm -f ${STAGE_DIR:?}/{config,credentials}
+
+## Copy over the remaining files from the staging directory into the director's home directory.
+scp  -F ${SSH_CONFIG_FILE:?} ${STAGE_DIR:?}/* install_director.sh director:
+
+
+# All done - make the zip file from the output directory and put it somewhere for temporary safe keeping
+## Edit the IdentityFile location to make it local to the ${OUTPUT_DIR:?}
+sed -i '' s@${OUTPUT_DIR:?}/@@ ${SSH_CONFIG_FILE:?}
 ZIPFILE=/tmp/${OWNER:?}.zip
-zip -j ${ZIPFILE:?} ${SSH_DIR:?}/*
+zip -j ${ZIPFILE:?} ${OUTPUT_DIR:?}/*
 
 message "Created zip file ${ZIPFILE:?} containing instructions for ${OWNER:?}"
-
-# message "Created ssh config file in ${SSH_CONFIG_FILE:?}. 
-# Execute 
-# 	ssh -qtF ${SSH_CONFIG_FILE:?} director 'cloudera-director bootstrap-remote analytic_cluster.conf --lp.remote.username=admin --lp.remote.password=admin' 
-# to create the analytic (permanent) cluster
-
-# When that is complete access HUE at http://${DIRECTOR_IP_ADDRESS:?}:8888, and enter the following command to make the output table:
-# 	CREATE EXTERNAL TABLE etl_table (d_year string,brand_id int,brand string,sum_agg float)  LOCATION 's3a://${BUCKET_NAME:?}/output'
-
-# Execute 
-# 	ssh -qtF ${SSH_CONFIG_FILE:?} director './run_all.sh\' 
-# to run the transient cluster etl job"
